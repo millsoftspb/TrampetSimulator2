a232cffdcdca930c56b88c695d74dab7d79cac3b
millsoftspb <millsoftspb@gmail.com>
2019-04-01 01:07:42 +0300

    continue animated start_activity and add intent of another activity

--- app/src/main/java/com/millsoftspb/trampetsimulator/StartActivity.java
+++ app/src/main/java/com/millsoftspb/trampetsimulator/StartActivity.java
@@ -1,8 +1,11 @@
 package com.millsoftspb.trampetsimulator;
 
+import android.content.Intent;
 import android.support.constraint.motion.MotionLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity {
 MotionLayout activity_start_layout;
@@ -10,12 +13,24 @@ MotionLayout activity_start_layout;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        activity_start_layout = findViewById(R.id.activity_start);
+        activity_start_layout = findViewById(R.id.activity_start_start);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         activity_start_layout.transitionToEnd();
+        Toast toast = Toast.makeText(getApplicationContext(),"start transition",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void start_play(View view) {
+        Intent intent= new Intent(this,PlayActivity.class);
+        startActivity(intent);
+    }
+
+    public void start_settings(View view) {
+        Intent intent= new Intent(this,SettingActivity.class);
+        startActivity(intent);
     }
 }
