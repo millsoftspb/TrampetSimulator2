e705e61fc3c92956a63ef4df98125ca480b863fc
kostusm <kostusm@gmail.com>
2019-03-31 23:07:56 +0300

    start animation start_activity

--- app/src/main/java/com/millsoftspb/trampetsimulator/StartActivity.java
+++ app/src/main/java/com/millsoftspb/trampetsimulator/StartActivity.java
@@ -1,13 +1,21 @@
 package com.millsoftspb.trampetsimulator;
 
+import android.support.constraint.motion.MotionLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class StartActivity extends AppCompatActivity {
-
+MotionLayout activity_start_layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        activity_start_layout = findViewById(R.id.activity_start);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        activity_start_layout.transitionToEnd();
     }
 }
