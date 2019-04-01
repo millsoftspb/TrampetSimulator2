e2b1de202723c94229ee494f160a5be40670098c
millsoftspb <millsoftspb@gmail.com>
2019-04-02 00:18:13 +0300

    start animation play_activity

--- app/src/main/java/com/millsoftspb/trampetsimulator/PlayActivity.java
+++ app/src/main/java/com/millsoftspb/trampetsimulator/PlayActivity.java
@@ -1,13 +1,54 @@
 package com.millsoftspb.trampetsimulator;
 
+import android.support.constraint.motion.MotionLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class PlayActivity extends AppCompatActivity {
-
+MotionLayout motion_play_layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        motion_play_layout = findViewById(R.id.motion_play_container);
+        motion_play_layout.setTransitionListener(new MotionLayout.TransitionListener() {
+            @Override
+            public void onTransitionStarted(MotionLayout motionLayout, int i, int i1) {
+
+            }
+
+            @Override
+            public void onTransitionChange(MotionLayout motionLayout, int i, int i1, float v) {
+
+            }
+
+            @Override
+            public void onTransitionCompleted(MotionLayout motionLayout, int i) {
+                  if (i==R.id.end_scene_playlayout)
+                      motion_play_layout.transitionToStart();
+
+            }
+
+            @Override
+            public void onTransitionTrigger(MotionLayout motionLayout, int i, boolean b, float v) {
+
+            }
+        });
+    }
+
+    public void onClick_button1(View view) {
+        motion_play_layout.transitionToEnd();
+
+    }
+
+    public void onClick_button3(View view) {
+        motion_play_layout.transitionToEnd();
+    }
+
+    public void onClick_button2(View view) {
+        motion_play_layout.transitionToEnd();
     }
 }
