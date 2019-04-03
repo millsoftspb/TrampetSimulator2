d02d325a1699761402b9a104705f38e7b10e7300
millsoftspb <millsoftspb@gmail.com>
2019-04-03 16:52:41 +0300

    continue animation play_activity, add background image in start and play activity

--- app/src/main/java/com/millsoftspb/trampetsimulator/PlayActivity.java
+++ app/src/main/java/com/millsoftspb/trampetsimulator/PlayActivity.java
@@ -3,16 +3,23 @@ package com.millsoftspb.trampetsimulator;
 import android.support.constraint.motion.MotionLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class PlayActivity extends AppCompatActivity {
-MotionLayout motion_play_layout;
+    MotionLayout motion_play_layout;
+    Button button1, button2, button3;
+    Boolean endAnimation = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
         motion_play_layout = findViewById(R.id.motion_play_container);
         motion_play_layout.setTransitionListener(new MotionLayout.TransitionListener() {
             @Override
@@ -27,9 +34,9 @@ MotionLayout motion_play_layout;
 
             @Override
             public void onTransitionCompleted(MotionLayout motionLayout, int i) {
-                  if (i==R.id.end_scene_playlayout)
-                      motion_play_layout.transitionToStart();
-
+                if (i == R.id.end_scene_playlayout)
+                    motion_play_layout.transitionToStart();
+                endAnimation =true;
             }
 
             @Override
@@ -37,18 +44,40 @@ MotionLayout motion_play_layout;
 
             }
         });
-    }
 
-    public void onClick_button1(View view) {
-        motion_play_layout.transitionToEnd();
+        button1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (endAnimation) {
+                    motion_play_layout.transitionToEnd();
+                    endAnimation = false;
+                }
+                return false;
+            }
+        });
 
-    }
+        button2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (endAnimation) {
+                    motion_play_layout.transitionToEnd();
+                    endAnimation = false;
+                }
+                return false;
+            }
+        });
 
-    public void onClick_button3(View view) {
-        motion_play_layout.transitionToEnd();
-    }
-
-    public void onClick_button2(View view) {
-        motion_play_layout.transitionToEnd();
+        button3.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (endAnimation) {
+                    motion_play_layout.transitionToEnd();
+                    endAnimation = false;
+                }
+                return false;
+            }
+        });
     }
 }
+
+
